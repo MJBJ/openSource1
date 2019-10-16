845753c6ae612355215266f46495e65a2f772c11
mzooh1112 <mzuhui001@gmail.com>
2019-10-16 21:28:19 +0900

    오픈소스 프로젝트 업로드

---
+++ cal.py
@@ -0,0 +1,40 @@
+# 계산기 만들기
+def sum:
+    # 덧셈 함수
+    pass
+
+def sub:
+    # 뺄샘 함수
+    pass
+
+def div:
+    # 나눗셈 함수
+    pass
+
+def mul:
+    # 곱셈 함수
+    pass
+
+if __name__=="__main__":
+    print("계산기를 실행합니다.")
+    
+    # 사용자에게 입력을 받는 input 함수
+    # operand1, operand2 라는 변수에 사용자의 입력값을 할당한다.
+    operand1 =
+    operand2 =
+
+    print("결과를 출력합니다.")
+
+    # 덧셈 결과를 출력한다.
+    print()
+
+    # 뺄셈 결과를 출력한다.
+    print()
+
+    # 나눗셈 결과를 출력한다.
+    print()
+
+    # 곱셈 결과를 출력한다.
+    print()
+
+    
